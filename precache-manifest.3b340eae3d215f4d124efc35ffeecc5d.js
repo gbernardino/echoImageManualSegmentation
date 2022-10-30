722e71ea7a65e431ba35ebe511f313a33ee6b063
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2022-10-30 22:53:44 +0100

    Updates

---
+++ precache-manifest.3b340eae3d215f4d124efc35ffeecc5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3fba2ea8fc0a67e90db92969027acb",
+    "url": "/echoImageManualSegmentation/index.html"
+  },
+  {
+    "revision": "5226eb166aeb9f8d378e",
+    "url": "/echoImageManualSegmentation/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "ee51d8d7cb3d65cd9cc1",
+    "url": "/echoImageManualSegmentation/static/css/main.1e780c7d.chunk.css"
+  },
+  {
+    "revision": "5226eb166aeb9f8d378e",
+    "url": "/echoImageManualSegmentation/static/js/2.1c40fe6a.chunk.js"
+  },
+  {
+    "revision": "ee51d8d7cb3d65cd9cc1",
+    "url": "/echoImageManualSegmentation/static/js/main.57abfdee.chunk.js"
+  },
+  {
+    "revision": "0aeb531ce34085db016f",
+    "url": "/echoImageManualSegmentation/static/js/runtime~main.d32f8aa6.js"
+  }
+]);
